d08f72d47efed1f8749e7334367c6156db0d706f
Yipeng Sun <syp@umd.edu>
2018-12-20 13:30:51 -0500

    Add header for easier import.

---
+++ MappingGen.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+#
+# License: BSD 2-clause
+# Last Change: Thu Dec 20, 2018 at 01:30 PM -0500
+
+import sys
+sys.path.insert(0, './pyUTM')
+
+from pyUTM.io import NestedListReader
