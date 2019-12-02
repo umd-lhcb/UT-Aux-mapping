05daac8dc54e81631d5b326493903e9c3e458bf1
Ben Flaggs <flaggsben@gmail.com>
2019-12-02 14:25:11 -0500

    Added mapping for inner BB to mirror BP

--- TelemetryMirrorBPMapping.py
+++ TelemetryMirrorBPMapping.py
@@ -19,8 +19,8 @@ from CometDcbMapping import post_filter_any
 from CometDcbMapping import make_comp_netname_dict
 
 mirror_bp_netlist = input_dir / Path("mirror_backplane.net")
-inner_bb_netlist = input_dir / Path("inner_bb.net")
-inner_bb_to_mirror_bp_mapping_filename = output_dir / Path("InnerBBMirrorBPMapping.csv")
+mirror_custom_bb_netlist = input_dir / Path("mirror_custom_telemetry_bb.net")
+mirror_bb_to_bp_mapping_filename = output_dir / Path("MirrorBBBackplaneMapping.csv")
 
 
 #####################
@@ -30,16 +30,14 @@ inner_bb_to_mirror_bp_mapping_filename = output_dir / Path("InnerBBMirrorBPMappi
 # Default net hopping should work for Custom BB.
 NetHopper = CurrentFlow()
 
-InnerBBReader = PcadReader(inner_bb_netlist)
-inner_bb_descr = InnerBBReader.read(NetHopper)
+MirrorCustomBBReader = PcadReader(mirror_custom_bb_netlist)
+mirror_custom_bb_descr = MirrorCustomBBReader.read(NetHopper)
 
 MirrorBPReader = PcadNaiveReader(mirror_bp_netlist)
 mirror_bp_descr = MirrorBPReader.read()
 
 # MirrorBPHopper = CurrentFlow([PLACE COMPONENTS TO TREAT AS COPPER HERE])
-# MirrorBPHopper = CurrentFlow([r"^R\d", r"^RT\d", r"^C\d"])
-#MirrorBPHopper = CurrentFlow([r"^R\d", r"^RT\d"])
-MirrorBPHopper = CurrentFlow([r"^RT\d"])
+MirrorBPHopper = CurrentFlow([r"^R\d", r"^RT\d", r"^C\d"])
 
 PcadReader.make_equivalent_nets_identical(
     mirror_bp_descr, MirrorBPHopper.do(mirror_bp_descr)
@@ -50,29 +48,20 @@ PcadReader.make_equivalent_nets_identical(
 # Filtering #
 #############
 
-#mirror_custom_bb_result = filter_comp(
-#    mirror_custom_bb_descr, r"^J_PT_\d+|^J_1V5_\d+|^J_2V5_\d+|^JT0$|^JT1$|^JT2$"
-#)
-
-inner_bb_result = filter_comp(
-    inner_bb_descr, r"^JP\d+|^JD\d+|^JPL0$|^JPL1$|^JPL2$"
+mirror_custom_bb_result = filter_comp(
+    mirror_custom_bb_descr, r"^J_PT_\d+|^J_1V5_\d+|^J_2V5_\d+|^JT0$|^JT1$|^JT2$"
 )
 
-#mirror_bp_result = filter_comp(mirror_bp_descr,
-#                               r"^JP\d|^JD\d|^JT0$|^JT1$|^JT2$")
 mirror_bp_result = filter_comp(mirror_bp_descr,
-                               r"^JP\d+|^JD\d+|^JPL0$|^JPL1$|^JPL2$")
+                               r"^JP\d|^JD\d|^JT0$|^JT1$|^JT2$")
 
-# Inner BB #####################################################################
+# Mirror Custom BB #############################################################
 # NOT USED IN FINDING CONNECTIONS (throw out GND later as GND is the current
 # problem)
 
-filter_inner_bb_throw_out = post_filter_any(
-    lambda x: x[1] not in ["9", "10", "16", "17", "18", "19", "20", "24", "25",
-                           "26", "27", "28", "29", "30"]
-)
-inner_bb_result_list = list(filter(filter_inner_bb_throw_out,
-                                   inner_bb_result))
+filter_bb_throw_out = post_filter_any(lambda x: x[1] not in ["S1", "S2", "29"])
+mirror_custom_bb_result_list = list(filter(filter_bb_throw_out,
+                                           mirror_custom_bb_result))
 
 # Mirror Backplane #############################################################
 # NOT USED IN FINDING CONNECTIONS (throw out GND later as GND is the current
@@ -86,37 +75,36 @@ mirror_bp_result_list = list(filter(filter_bp_throw_gnd, mirror_bp_result))
 # Find Mirror BB to Mirror Backplane Connections #
 ##################################################
 
-# Inner BB -> Mirror Backplane ################################################
+# Mirror Custom Telemetry BB -> Mirror Backplane ###############################
 
-inner_bb_ref = make_comp_netname_dict(inner_bb_descr)
+mirror_bb_ref = make_comp_netname_dict(mirror_custom_bb_descr)
 mirror_bp_ref = make_comp_netname_dict(mirror_bp_descr)
 
-compnames_inner_bb = inner_bb_ref.keys()
-netnames_inner_bb = inner_bb_ref.values()
-list_comp_inner_bb = list(compnames_inner_bb)
-list_nets_inner_bb = list(netnames_inner_bb)
-
+compnames_mirror_bb = mirror_bb_ref.keys()
+netnames_mirror_bb = mirror_bb_ref.values()
+list_comp_bb = list(compnames_mirror_bb)
+list_nets_bb = list(netnames_mirror_bb)
 
 compnames_mirror_bp = mirror_bp_ref.keys()
 netnames_mirror_bp = mirror_bp_ref.values()
 list_comp_bp = list(compnames_mirror_bp)
 list_nets_bp = list(netnames_mirror_bp)
 
-inner_bb_to_mirror_bp_map = []
+mirror_bb_to_mirror_bp_map = []
 
-for i in range(len(list_nets_inner_bb)):
+for i in range(len(list_nets_bb)):
     row = []
     for j in range(len(list_nets_bp)):
         if (
-            list_comp_inner_bb[i][0] == list_comp_bp[j][0] and
-            list_comp_inner_bb[i][1] == list_comp_bp[j][1]
+            list_comp_bb[i][0] == list_comp_bp[j][0] and
+            list_comp_bb[i][1] == list_comp_bp[j][1]
         ):
 
-            row.append(list_nets_inner_bb[i])
-            row.append("-".join(list_comp_inner_bb[i]))
+            row.append(list_nets_bb[i])
+            row.append("-".join(list_comp_bb[i]))
             row.append(list_nets_bp[j])
 
-            inner_bb_to_mirror_bp_map.append(row)
+            mirror_bb_to_mirror_bp_map.append(row)
 
 # THE ABOVE WORKS ALTHOUGH IT SAYS THAT SOME NETS GO TO GROUND WHEN THEY
 # SHOULDN'T As of now, I know that the "..._2V5_SENSE_..." lines all go to GND
@@ -128,11 +116,11 @@ for i in range(len(list_nets_inner_bb)):
 # Output to csv #
 #################
 
-# Inner BB -> Mirror Backplane (short?) ######################
+# Mirror Custom Telemetry BB -> Mirror Backplane (short?) ######################
 
 write_to_csv(
-    inner_bb_to_mirror_bp_mapping_filename,
-    inner_bb_to_mirror_bp_map,
-    ["Inner BB net", "Inner BB connector",
+    mirror_bb_to_bp_mapping_filename,
+    mirror_bb_to_mirror_bp_map,
+    ["Mirror Telemetry BB net", "Telemetry BB connector",
      "Mirror Backplane net"],
 )
