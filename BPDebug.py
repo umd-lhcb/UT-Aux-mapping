e83f9ed726e25d0e627dfa65ce65efa8ebdbd580
Yipeng Sun <syp@umd.edu>
2019-12-13 14:50:43 -0500

    Only hop through R and RSP.

--- BPDebug.py
+++ BPDebug.py
@@ -2,7 +2,9 @@
 #
 # Author: Yipeng Sun
 # License: BSD 2-clause
-# Last Change: Fri Dec 13, 2019 at 02:24 PM -0500
+# Last Change: Fri Dec 13, 2019 at 02:50 PM -0500
+
+import re
 
 from pathlib import Path
 
@@ -17,10 +19,29 @@ from CometDcbMapping import input_dir
 true_bp_netlist = input_dir / Path("mirror_backplane.net")
 
 
+###########
+# Helpers #
+###########
+
+def filter_netname(nets, name):
+    for n in nets:
+        if re.search(name, n):
+            return True
+    return False
+
+
+def netname_printout(nets, name):
+    for n in nets:
+        if re.search(name, n):
+            print(n)
+
+
 #################
 # True BP debug #
 #################
 
 TrueBPReader = PcadNaiveReader(true_bp_netlist)
-TrueBPHopper = CurrentFlow([r'^R\d+', r'^RBSP_\d+', r'^RB_\d+', r'^RSB_\d+'])
-true_nets_hopped = TrueBPHopper.do()
+true_bp_descr = TrueBPReader.read()
+
+TrueBPHopper = CurrentFlow([r'^R\d+', r'^RSP_\d+'])
+true_nets_hopped = TrueBPHopper.do(true_bp_descr)
