8b6403d336b5b9ed3b41caaea775228499c9191c
Yipeng Sun <syp@umd.edu>
2020-01-09 05:43:03 -0500

    Generate mapping for all U.FL connectors on PF.

--- CometPFMapping.py
+++ CometPFMapping.py
@@ -2,7 +2,7 @@
 #
 # Author: Yipeng Sun
 # License: BSD 2-clause
-# Last Change: Thu Jan 09, 2020 at 05:29 AM -0500
+# Last Change: Thu Jan 09, 2020 at 05:41 AM -0500
 
 from pathlib import Path
 
@@ -12,11 +12,10 @@ sys.path.insert(0, './pyUTM')
 from pyUTM.io import write_to_csv
 
 from UT_Aux_mapping.const import output_dir
-from UT_Aux_mapping.helpers import filter_comp, make_comp_netname_dict
-from UT_Aux_mapping.helpers import make_comp_comp_dict
+from UT_Aux_mapping.helpers import filter_comp, make_comp_comp_dict
 
-from CometDcbMapping import path_finder_descr, comet_j1_j2_duo_to_fpga, \
-    filter_path_finder_throw_gnd
+from CometDcbMapping import path_finder_descr, \
+    comet_j1_j2_duo_to_fpga, filter_path_finder_throw_gnd
 
 comet_pf_mapping_filename = output_dir / Path('CometPFMapping.csv')
 
@@ -24,8 +23,6 @@ comet_pf_mapping_filename = output_dir / Path('CometPFMapping.csv')
 #############
 # Filtering #
 #############
-
-path_finder_ref = make_comp_netname_dict(path_finder_descr)
 
 # We care about JN (U.FL connectors) to COMET
 path_finder_result = filter_comp(path_finder_descr, r'^COMET|^J\d+$')
@@ -40,7 +37,7 @@ path_finder_result = list(filter(filter_path_finder_throw_gnd,
 path_finder_connections_raw = [
     make_comp_comp_dict(path_finder_result, j, c)
     for c in ['COMET_A_J1', 'COMET_A_J2', 'COMET_B_J1', 'COMET_B_J2']
-    for j in ['J1', 'J2', 'J3', 'J4', 'J5']
+    for j in ['J'+str(k) for k in range(1, 38)]
 ]
 
 # Again, combine into a single dictionary.
@@ -59,15 +56,12 @@ comet_ufl_data = []
 for ufl_pin, comet_pin in path_finder_ufl_to_comet.items():
     row = []
 
-    row.append(path_finder_ref[ufl_pin])
+    fpga_pin = comet_j1_j2_duo_to_fpga[comet_pin]
+    row.append('-'.join(fpga_pin))
 
     row.append('-'.join(ufl_pin))
     row.append('-'.join(comet_pin))
 
-    fpga_pin = comet_j1_j2_duo_to_fpga[comet_pin]
-    row.append('-'.join(fpga_pin))
-
-    row.reverse()
     comet_ufl_data.append(row)
 
 
@@ -80,5 +74,5 @@ for ufl_pin, comet_pin in path_finder_ufl_to_comet.items():
 write_to_csv(
     comet_pf_mapping_filename, comet_ufl_data,
     ['COMET FPGA pin', 'Pathfinder COMET connector',
-     'Pathfinder U.FL connector', 'Signal ID']
+     'Pathfinder U.FL connector']
 )
